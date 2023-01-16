328d63de0f6b7369cc3362ed978d79e76bec9cc1
Ene44-A <xjorgex2011@gmail.com>
2023-01-15 21:10:01 -0500

    commit nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/navBar.css';
+
+const NavBar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Inicio</li>
+            <li>Habilidades</li>
+            <li>Portafolio</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+  );
+}
+
+export default NavBar;
