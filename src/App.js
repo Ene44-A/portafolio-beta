3c63f420764325f479a393e7ee8caf6756715bc3
Ene44-A <xjorgex2011@gmail.com>
2023-01-16 19:46:38 -0500

    commit 0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <header className="header">
+        <di className='card-cont'>
+          <div className="photo-hero"></div>
+          <div className="title-hero">
+            <h3>Hi!</h3>
+            <h3>I'm <span> Andrés García</span></h3>
+            <h3>Frontend Developer</h3>
+          </div>
+        </di>
+      </header>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import '../assets/navBar.css';
+import React from "react";
+import "../assets/navBar.css";
+import { AiOutlineMenu, AiOutlineLaptop } from "react-icons/ai";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div>
-        <ul>
-            <li>Inicio</li>
-            <li>Habilidades</li>
-            <li>Portafolio</li>
-            <li>Contacto</li>
-        </ul>
-    </div>
+    <nav className="nav-cont">
+      <di className="nav-bar-main">
+        <div className="title-nav">
+          <AiOutlineLaptop />
+          <h1 className="title-h1">Andrés García</h1>
+        </div>
+        <button
+          className="button-nav"
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+            console.log("action button");
+          }}
+        >
+          <AiOutlineMenu />
+        </button>
+      </di>
+
+      <ul className={isExpanded ? null : "OffExpanded"}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   );
-}
+};
 
 export default NavBar;
