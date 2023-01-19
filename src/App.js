f3d143b13772f7fda16f3ea959ac0ec1f76230b0
Ene44-A <xjorgex2011@live.com>
2023-01-19 11:18:45 -0600

    commit newPc

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import "./App.css";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Services from "./components/Services";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="header">
-        <di className='card-cont'>
+        <motion.div
+        initial={{opacity:0, scale:0.5}}
+        animate={{opacity:1, scale:1}}
+        transition={{duration:0.5}}
+        className='card-cont'>
           <div className="photo-hero"></div>
           <div className="title-hero">
             <h3>Hi!</h3>
             <h3>I'm <span> Andrés García</span></h3>
             <p>Frontend Developer</p>
           </div>
-        </di>
+        </motion.div>
       </header>
       <About />
       <Services />
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../assets/services.css';
 import { SlLike, SlBulb, SlScreenDesktop, SlPeople, SlSpeedometer, SlTarget} from "react-icons/sl";
+import { motion } from "framer-motion";
+
 
 const Services = () => {
   return (
@@ -8,36 +10,50 @@ const Services = () => {
         <h3>Services</h3>
         <p>Services I offer to my clients</p>
         <div className='services-cont'>
-            <div className='service-cont'>
+            <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+             className='service-cont'>
                 <div className='icon-service'><SlLike /></div>
                 <h4>Fully Responsive</h4>
                 <p>You site will display properly on any divice, including desktop computers, tablets and mobile phone.</p>
-            </div>
-            <div className='service-cont'>
+            </motion.div>
+            <motion.div
+            
+             className='service-cont'>
                 <div className='icon-service'><SlBulb /></div>
                 <h4>Creative Design</h4>
                 <p>A good and attractive web design helps you keep your leads on your site, which is the digital face of your business.</p>
-            </div>
-            <div className='service-cont'>
+            </motion.div>
+            <motion.div
+
+             className='service-cont'>
                 <div className='icon-service'><SlScreenDesktop /></div>
                 <h4>Development</h4>
                 <p>I know the importance of web design and can help you create a website you'll love.</p>
-            </div>
-            <div className='service-cont'>
+            </motion.div>
+            <motion.div
+
+             className='service-cont'>
                 <div className='icon-service'><SlPeople /></div>
                 <h4>Social Media Integration</h4>
                 <p>there are a lot of social platforms out there, and you should promote your presence on them on your website.</p>
-            </div>
-            <div className='service-cont'>
+            </motion.div>
+            <motion.div
+
+             className='service-cont'>
                 <div className='icon-service'><SlSpeedometer /></div>
                 <h4>Performence</h4>
                 <p>Retaining users is crucial to improving conversions. High-performing sites engage and retain users.</p>
-            </div>
-            <div className='service-cont'>
+            </motion.div>
+            <motion.div
+
+             className='service-cont'>
                 <div className='icon-service'><SlTarget /></div>
                 <h4>SEO</h4>
                 <p>I will optimize your site with intelligent Search Engine Optimization strategy to generate leads.</p>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
