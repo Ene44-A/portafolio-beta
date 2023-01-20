c5900dafcd3a5749f06d253b11255b42271d8e96
Ene44-A <xjorgex2011@live.com>
2023-01-19 21:08:52 -0500

    commit motion framer

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,44 +11,54 @@ const Services = () => {
         <p>Services I offer to my clients</p>
         <div className='services-cont'>
             <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            viewport={{once:true, amount:0.8}}
              className='service-cont'>
-                <div className='icon-service'><SlLike /></div>
+                <div className='icon-service'><SlLike /></div> 
                 <h4>Fully Responsive</h4>
                 <p>You site will display properly on any divice, including desktop computers, tablets and mobile phone.</p>
             </motion.div>
             <motion.div
-            
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            viewport={{once:true, amount:0.8}}
              className='service-cont'>
                 <div className='icon-service'><SlBulb /></div>
                 <h4>Creative Design</h4>
                 <p>A good and attractive web design helps you keep your leads on your site, which is the digital face of your business.</p>
             </motion.div>
             <motion.div
-
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            viewport={{once:true, amount:0.8}}
              className='service-cont'>
                 <div className='icon-service'><SlScreenDesktop /></div>
                 <h4>Development</h4>
                 <p>I know the importance of web design and can help you create a website you'll love.</p>
             </motion.div>
             <motion.div
-
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            viewport={{once:true, amount:0.8}}
              className='service-cont'>
                 <div className='icon-service'><SlPeople /></div>
                 <h4>Social Media Integration</h4>
                 <p>there are a lot of social platforms out there, and you should promote your presence on them on your website.</p>
             </motion.div>
             <motion.div
-
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            viewport={{once:true, amount:0.8}}
              className='service-cont'>
                 <div className='icon-service'><SlSpeedometer /></div>
                 <h4>Performence</h4>
                 <p>Retaining users is crucial to improving conversions. High-performing sites engage and retain users.</p>
             </motion.div>
             <motion.div
-
+            initial={{opacity:0}}
+            whileInView={{opacity:1}}
+            viewport={{once:true, amount:0.8}}
              className='service-cont'>
                 <div className='icon-service'><SlTarget /></div>
                 <h4>SEO</h4>
